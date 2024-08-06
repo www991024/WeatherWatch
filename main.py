9d8d425c9847bc80a62749f0aed2ae3ef2cb7cc2
Hao Tai <www991024@gmail.com>
2024-08-06 22:08:19 +0800

    first commit

--- main.py
+++ main.py
@@ -7,24 +7,3 @@ async def root():
     return {"message":"Hello World"}
 
 
-conn = get_connection()
-
-if conn:
-    try:
-        # 创建游标对象
-        cur = conn.cursor()
-
-        # 执行查询
-        cur.execute("SELECT * FROM mytable;")
-        rows = cur.fetchall()
-
-        # 处理结果
-        for row in rows:
-            print(row)
-
-        # 关闭游标和连接
-        cur.close()
-    except Exception as e:
-        print(f"发生错误: {e}")
-    finally:
-        conn.close()
